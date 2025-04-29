a564018cb2b2872fb7d28b376f3e5aaa15117d9f
Zulkarinine Agbeleshola <zulkarininecute@gmail.com>
2025-04-29 17:24:26 +0100

    New Edit

--- Assignment.js
+++ Assignment.js
@@ -1,29 +1,136 @@
-// let number=[1,2,3,4]
-// const double=number.map(number=>number*2)
-// // console.log(double)
+// let numbers=[1, 2, 3, 4]
+// const doubled=numbers.map(num=>num * 2)
+// console.log(doubled)
 
-// let nums=[5, 12, 8, 130, 44]
-// let greaterThan10=nums.filter(num=>num>10)
-// // console.log(greaterThan10)
+// let numbers=[5, 12, 8, 130, 44]
+// const numsGreaterThan10=numbers.filter(nums=>nums>10)
+// console.log(numsGreaterThan10)
 
-// let userLists=[
-//     {name: 'John', active: true}, {name: 'Mary', active: false}
+// let productPrice=[50,100,150,200,250]
+// const discountprice=productPrice.map(price=>price*0.90)
+// console.log(discountprice)
+
+// let userList=[{ name: 'John', active: true }, { name: 'Mary', active: false }]
+// const activeUser=userList.filter(user=>user.active)
+// console.log(activeUser)
+
+// let numbers=[3, 6, 9]
+// const plusOne=numbers.map(nums=>nums+1)
+// console.log(plusOne)
+
+// let numbers=[12, 5, 8, 130, 44]
+// const lessThan10=numbers.filter(nums=>nums<10)
+// console.log(lessThan10)
+
+// let cart=["book","pen","shirt","cream","bag"]
+//     const findItem=cart.filter(cart=>cart.includes("book"))
+// console.log(findItem)
+
+// let playerNames=["Gerrad","Mikel","Suarez","Adam","Diaz"]
+// const upperCased=playerNames.map(name=>name.toLocaleUpperCase())
+// console.log(upperCased)
+
+// let numbers=[22, 35, 64, 23, 90]
+// const greaterThan30=numbers.filter(num=>num>30)
+// console.log(greaterThan30)
+
+// let cities=["lagos","benin","ogun","edo"]
+// const upperCasedCities=cities.map(city=>city.toLocaleUpperCase())
+// console.log(upperCasedCities)
+
+// let myCart=['phone', 'tablet', 'laptop']
+//     const findItem=myCart.filter(cart=>cart.includes("laptop"))
+// console.log(findItem)
+
+// let Library=["Good Omens","Neverwhere","The Night Circus"]
+// Library.push("The Hobbit")
+// console.log(Library)
+
+// let scores=[80, 55, 60, 90, 45]
+// const scoresAbove60=scores.filter(score=>score>60)
+// console.log(scoresAbove60)
+
+// let response=[true, false, true, false]
+// const yesOrNo=response.map(response=>response? "yes":"No")
+// console.log(yesOrNo)
+
+// let ages=[18, 22, 15, 30, 25]
+// const olderThan21=ages.filter(age=>age>=21)
+// console.log(olderThan21)
+
+// let notification=["wale sent you a snap","recieve a N300,000 in gift appreciation","chelsea targeting Rogers"]
+// // notification.unshift("liverpool are premier league champions")
+// // console.log(notification)
+
+// let emailList=["wale@example.com","dele@example.com","sade@example.com","jack@example.com","felix@example.com"]
+//     const findEmail=emailList.filter(email=>email.includes("jack@example.com"))
+// console.log(findEmail)
+
+// let productPrice=[12.5, 5.99, 100.0, 7.99]
+// let shippingFee=2
+// const totalPrice=productPrice.map(price=>price+shippingFee)
+// console.log(totalPrice)
+
+// let numbers=[2, 4, 6, 8, 10]
+// const divisible=numbers.filter(nums=>nums%4===0)
+// console.log(divisible)
+
+// let number=[1, 2, 3, 4, 5]
+// const sqaured=number.map(num=>num**2)
+// console.log(sqaured)
+
+// let price=[50,100,150,200,250]
+// const priceGreaterThan50=price.filter(price=>price>50)
+// console.log(priceGreaterThan50)
+
+// let sensorReadings=[45, -3, 67, 20, -1]
+// const lessThan0=sensorReadings.filter(sensor=>sensor<0)
+// console.log(lessThan0)
+
+// let customerList=["dan","serah","esther","julian","edeth","eshiet"]
+// const findName=customerList.filter(name=>name.includes("an"))
+// console.log(findName)
+
+// let price=[12.5, 5.99, 100.0, 7.99]
+// let tax= 1.05
+// const totalPrice=price.map(price=>price*tax)
+// console.log(totalPrice)
+
+// let numbers=[4, 9, 16, 25]
+// const sqaured=numbers.map(num=>num**2)
+// console.log(sqaured)
+
+// let mailList=["wale@example.com","dele@example.com","sade@example.com","jack@example.com","felix@example.com"]
+// let removeduser=mailList.filter(mail=>mail!=="sade@example.com")
+// mailList=removeduser
+// console.log(mailList)
+
+// const students = [
+//     { name: 'Felix', grade: 'A' },
+//     { name: 'Ayo', grade: 'B' },
+//     { name: 'Emmanuel', grade: 'A' },
+//     { name: 'David', grade: 'C' }
+//   ];
+
+//   let gradeA=students.filter(student=>student.grade.includes("A")) 
+//   console.log(gradeA)
+
+// const leaderBoard=[
+//     {username:"felix"},
+//     {username:"dele"},
+//     {username:"toyin"}
 // ]
-// let activeUser=userLists.filter(userLists=>userLists.active)
-// // console.log(activeUser)
-// // // console.log(userLists)
 
-// let digits=[3,6,9]
-// let digitPlus1=digits.map(digit=>digit+1)
-// console.log(digitPlus1)
+// let userUpperCased=leaderBoard.map(board=>board.username.toLocaleUpperCase())
+// console.log(userUpperCased)
 
-// let nums=[12, 5, 8, 130, 44]
-// let numLessThan10=nums.filter(num=>num<10)
-// console.log(numLessThan10)
+// let temperature=[32, 45, 28, 50, 38]
+// const tempBelow40=temperature.filter(temp=>temp<40)
+// console.log(tempBelow40)
 
-// MyCart=["bag","shoe","book","pen","tie"]
-// console.log(MyCart.includes("book"))
+// let product=[{Item:"bag",Price:50}, {Item:"doll",Price:150},{Item:"charger",Price:90},{Item:"laptop",Price:350},{Item:"phone",Price:200}]
+// let costAbove100=product
+// .map(product=>product.Price)
+// .find(product=>product>100)
+// console.log(costAbove100)
 
-PlayerNames=["trent","andrew","thiago","jone","elliot"]
-PlayerNameUpperCase=PlayerNames.map(PlayerNames=>PlayerNames.toUpperCase())
-console.log(PlayerNameUpperCase)
